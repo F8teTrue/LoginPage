5b0e0d0edc0874b6662227d228f0ebcf0d327a41
F8teTrue <preben.dahl2006@gmail.com>
2024-05-12 18:14:07 +0200

    Login Page
    Very simple login page. No security.

---
+++ Firebase login/login.js
@@ -0,0 +1,42 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBATT-7pmmb-O7E5e1D-KczrBOSvc75at0",
+    authDomain: "loginpage-93a16.firebaseapp.com",
+    projectId: "loginpage-93a16",
+    storageBucket: "loginpage-93a16.appspot.com",
+    messagingSenderId: "917081627931",
+    appId: "1:917081627931:web:91cf68e2aa70cbb41807be"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+async function login(username, password) {
+    try {
+        const checkUsername = await db.collection("users").where("username", "==", username).get();
+        if (checkUsername.empty) {
+            alert("Username does not exist")
+            return
+        }
+        
+        const userDoc = checkUsername.docs[0]
+        if (userDoc.data().password !== password) {
+            alert("Invalid username or password")
+            return
+        }
+
+        console.log("User has been logged in")
+        window.location.href = "homePage.html"
+    } catch (error) {
+        console.log("Error logging in user", error)
+    }
+}
+
+document.getElementById("loginForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    await login(username, password)
+});
---
+++ Firebase login/signUp.js
@@ -0,0 +1,50 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBATT-7pmmb-O7E5e1D-KczrBOSvc75at0",
+    authDomain: "loginpage-93a16.firebaseapp.com",
+    projectId: "loginpage-93a16",
+    storageBucket: "loginpage-93a16.appspot.com",
+    messagingSenderId: "917081627931",
+    appId: "1:917081627931:web:91cf68e2aa70cbb41807be"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+async function signUp(username, password) {
+    try {
+        if (username.length < 5) {
+            alert("Username must be at least 5 characters long");
+            return;
+        }
+        if (password.length < 8) {
+            alert("Password must be at least 8 characters long");
+            return;
+        }
+        
+        const checkUsername = await db.collection("users").where("username", "==", username).get();
+        if (!checkUsername.empty) {
+            alert("Username already taken");
+            return;
+        }
+
+        await db.collection("users").add({
+            username: username,
+            password: password
+        });
+
+        console.log("User has been created");
+        window.location.href = "login.html"
+    } catch (error) {
+        console.log("Error creating user", error);
+    }
+}
+
+document.getElementById("signUpForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value; 
+    const password = document.getElementById("password").value;
+
+    await signUp(username, password);
+});
